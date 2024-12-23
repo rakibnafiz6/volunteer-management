37a5a53de6f0a7ce86da508c135a89eff00a4b9a
nafiz rakib <rakibnafiz6@gmail.com>
2024-12-23 10:54:08 +0600

    added the volunteer needs now section

---
+++ src/components/VolunteerNeed/VolunteerNeed.jsx
@@ -0,0 +1,45 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const VolunteerNeed = () => {
+    const [volunteerNow, setVolunteerNow] = useState([]);
+
+    useEffect(()=>{
+        axios.get(`${import.meta.env.VITE_API_URL}/volunteer-need`)
+        .then(res =>{
+            setVolunteerNow(res.data);
+        })
+    },[])
+
+    return (
+        <div>
+            <h2>volunteer need now section</h2>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+            {
+                volunteerNow.map(volunteer=><div key={volunteer._id} className="card bg-base-100 shadow-xl">
+                    <figure className=''>
+                        <img
+                            className='w-80 h-60'
+                            src={volunteer.thumbnail}
+                            alt="volunteer" />
+                    </figure>
+                    <p className='ml-5 text-[#091854]'>Deadline: <span className='text-red-500'>{volunteer.deadline}</span></p>
+                    <div className="pl-5 mt-4 space-y-2">
+                        <h2 className="card-title text-[#091854]">{volunteer.title}</h2>
+                        <p className='text-gray-400'>{volunteer.category}</p>
+                        <div className="card-actions pb-5">
+                            <button class="btn btn-outline text-[#274dcf] rounded-full hover:bg-[#274dcf] mt-4">View Details</button>
+                        </div>
+                    </div>
+                </div>)
+            }
+        </div>
+       <div className='flex justify-center mt-10'>
+       <Link to='/allVolunteer'><button className='btn bg-neutral text-white w-[450px]'>See All</button></Link>
+       </div>
+        </div>
+    );
+};
+
+export default VolunteerNeed;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Banner from '../components/Banner/Banner';
+import VolunteerNeed from '../components/VolunteerNeed/VolunteerNeed';
 
 const Home = () => {
     return (
         <div>
         <Banner></Banner>
+        <section>
+            <VolunteerNeed></VolunteerNeed>
+        </section>
         </div>
     );
 };

8985e2af413e6663d6e950e20f3eae962806ce68
nafiz rakib <rakibnafiz6@gmail.com>
2024-12-24 00:34:49 +0600

    added the delete button functionality

--- src/pages/ManageMyVolunteer.jsx
+++ src/pages/ManageMyVolunteer.jsx
@@ -2,6 +2,7 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import useAuth from '../hook/useAuth';
 import { Link } from 'react-router-dom';
+import Swal from 'sweetalert2';
 
 const ManageMyVolunteer = () => {
     const [myPost, setMyPost] = useState([]);
@@ -14,6 +15,35 @@ const ManageMyVolunteer = () => {
             })
 
     }, [user?.email])
+
+    const handleDelete = (id)=>{
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+          }).then((result) => {
+            if (result.isConfirmed) {
+        axios.delete(`${import.meta.env.VITE_API_URL}/volunteers/${id}`)
+        .then(res =>{
+               if(res.data.deletedCount){
+                Swal.fire({
+                    title: "Deleted!",
+                    text: "Your post has been deleted.",
+                    icon: "success"
+                  });
+                const remainingPost = myPost.filter(post => post._id !== id);
+                setMyPost(remainingPost);
+
+               }
+        })
+
+    }
+    });
+    }
 
 
     return (
@@ -40,7 +70,7 @@ const ManageMyVolunteer = () => {
                                 <td>{post.category}</td>
                                 <td>
                                     <Link to={`/update/${post._id}`}><button className='btn mr-2'>Update</button></Link>
-                                    <button className='btn'>Delete</button>
+                                    <button onClick={()=>handleDelete(post._id)} className='btn'>Delete</button>
                                 </td>
                             </tr>)
                         }
